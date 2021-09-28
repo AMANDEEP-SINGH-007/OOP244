53e201a3cd17d4a65e54450bc4135d2b9ce3f07e
AMANDEEP-SINGH-007 <asingh984@myseneca.ca>
2021-09-27 22:10:54 -0400

    reflect.txt added

--- LAB2/part2/part2/Guests.cpp
+++ LAB2/part2/part2/Guests.cpp
@@ -1,3 +1,13 @@
+/*
+    Name   : - Amandeep Singh
+    ID     : 145041208
+    Email  : asingh984@myseneca.ca
+    Section: OOP244 NCC
+    Date   : OCT 18,2021
+
+    I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
@@ -56,7 +66,7 @@ namespace sdds {
             G_DATA[i] = theGuestList.m_gst[i];
         }
 
-        G_DATA[theGuestList.m_noOfGuests] = aGuest;
+        G_DATA[theGuestList.m_noOfGuests] = aGuest; //Professor's help (only this line, not the whole function)
         delete[] theGuestList.m_gst;
         theGuestList.m_gst = G_DATA;
         theGuestList.m_noOfGuests += 1;
